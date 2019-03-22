8eaa34b8baf80169dd58871c180be217fd12177f
Zach Ferland <zachferland@gmail.com>
2019-03-22 10:02:54 -0400

    fix: match orbit cache data model, cache/namespace for each store

--- src/Cache.js
+++ src/Cache.js
@@ -1,4 +1,5 @@
 const redis = require("redis")
+const path = require('path')
 
 const encode = (value) => (typeof value === 'string' ? value : JSON.stringify(value))
 const decode = (value) => {
@@ -6,10 +7,14 @@ const decode = (value) => {
   return value
 }
 
+// Keys are namespaced by db name, since in orbit each store gets its own cache
+const keyWrap = (prefix, key) => `${prefix}_${key}`
+
 class Cache {
-  constructor (opts) {
+  constructor (storage, dbAddress, opts) {
     this.opts = opts
-    this.store = redis.createClient(this.opts);
+    this.store = storage
+    this.dbAddress = dbAddress
   }
 
   async open () { }
@@ -22,25 +27,33 @@ class Cache {
 
   get (key) {
     return new Promise((resolve, reject) => {
-      this.store.get(key, (err, reply) => {
+      this.store.get(keyWrap(this.dbAddress, key), (err, reply) => {
         resolve(decode(reply))
       })
     })
   }
 
   async set (key, value) {
-    this.store.set(key, encode(value))
+    this.store.set(keyWrap(this.dbAddress, key), encode(value))
   }
 
   async del (key) {
-    this.store.del(key)
+    this.store.del(keyWrap(this.dbAddress, key))
   }
 }
 
 const Start = (opts) => {
+
+  let cache
+
   return {
      load: async (directory, dbAddress) => {
-       return new Cache(opts)
+       // directory not used here for redis
+      if (!cache) {
+        cache = redis.createClient(this.opts)
+      }
+      const dbPath = path.join(dbAddress.root, dbAddress.path)
+      return new Cache(cache, dbPath)
      },
      close: async () => {
      },
